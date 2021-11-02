fff62fd8cf640c980ed2bed562c3ba54a2ee2bcb
unknown <daza.ronnie.09212002@gmail.com>
2021-11-02 09:41:10 +0800

    Ask for name Commit

---
+++ PLD Assignment 3 - Part 1.py
@@ -0,0 +1,11 @@
+# Create a program that will ask for name, age, and address.
+# Display those details in the following format.
+# Hi, my name is __________. I am __________ years old and I live in __________.
+
+def getName():
+    _name = input("Enter your name: ")
+    return _name
+
+# Steps
+# 1. Ask for name then save to variable.
+name = getName()
