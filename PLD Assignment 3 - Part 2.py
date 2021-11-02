dc89986aee346060139eaf74c76f791e304df825
unknown <daza.ronnie.09212002@gmail.com>
2021-11-02 09:56:31 +0800

    Ask how many apples you want to buy Commit

---
+++ PLD Assignment 3 - Part 2.py
@@ -0,0 +1,12 @@
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if the apple price is 20 pesos and the orange price is 25 pesos.
+# Display the output in the following format.
+# The total amount is __________.
+
+def howmanyApples():
+    _appleQuestion = int(input("How many apples you want to buy? "))
+    return _appleQuestion
+
+# Steps
+# 1. Ask how many apples you want to buy then save to variable.
+appleQuestion = howmanyApples
